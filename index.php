02535d504809e061b1d96f565fbd84a22798f6fc
sachin-shetty05 <54214367+sachin-shetty05@users.noreply.github.com>
2020-08-31 20:20:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -15,17 +15,17 @@ $json_external_user_id = json_encode("test_user");
 $json_first_name = json_encode("John");
 $json_last_name = json_encode("Doe");
 $json_permissions = json_encode( array ( "see_user_dashboards", "see_lookml_dashboards", "access_data", "see_looks" ) );
-$json_models = json_encode( array ( "covid_cci" ) );
-//$json_group_ids = json_encode( array ( 4, 2 ) );  // just some example group ids
-//$json_external_group_id = json_encode("awesome_engineers");
-//$json_user_attributes = json_encode( array ( "an_attribute_name" => "my_value", "my_number_attribute" => "0.231" ) );  // just some example attributes
+$json_models = json_encode(array());
+$json_group_ids = json_encode(array());  // just some example group ids
+$json_external_group_id = json_encode("");
+$json_user_attributes = json_encode( array () );  // just some example attributes
 // NOTE: accessfilters must be present and be a json hash. If you don't need access filters then the php
 // way to make an empty json hash as an alternative to the below seems to be:
-// $accessfilters = new stdClass()
+$accessfilters = new stdClass();
 //$accessfilters = array (
 //  "<your_model_name>"  =>  array ( "view_name.dimension_name" => "<value>" )
 //);
-//$json_accessfilters = json_encode($accessfilters);
+$json_accessfilters = json_encode($accessfilters);
 
 $stringtosign = "";
 $stringtosign .= $host . "\n";
@@ -35,11 +35,11 @@ $stringtosign .= $json_current_time . "\n";
 $stringtosign .= $json_session_length . "\n";
 $stringtosign .= $json_external_user_id . "\n";
 $stringtosign .= $json_permissions . "\n";
-$stringtosign .= $json_models;
-//$stringtosign .= $json_group_ids . "\n";
-//$stringtosign .= $json_external_group_id . "\n";
-//$stringtosign .= $json_user_attributes . "\n";
-//$stringtosign .= $json_accessfilters;
+$stringtosign .= $json_models. "\n";
+$stringtosign .= $json_group_ids . "\n";
+$stringtosign .= $json_external_group_id . "\n";
+$stringtosign .= $json_user_attributes . "\n";
+$stringtosign .= $json_accessfilters;
 
 $signature = trim(base64_encode(hash_hmac("sha1", utf8_encode($stringtosign), $secret, $raw_output = true)));
 // , $raw_output = true
@@ -51,10 +51,10 @@ $queryparams = array (
     'external_user_id'  =>  $json_external_user_id,
     'permissions' =>  $json_permissions,
     'models'  =>  $json_models,
-    //'group_ids' => $json_group_ids,
-    //'external_group_id' => $json_external_group_id,
-    //'user_attributes' => $json_user_attributes,
-    //'access_filters'  =>  $json_accessfilters,
+    'group_ids' => $json_group_ids,
+    'external_group_id' => $json_external_group_id,
+    'user_attributes' => $json_user_attributes,
+    'access_filters'  =>  $json_accessfilters,
     'first_name'  =>  $json_first_name,
     'last_name' =>  $json_last_name,
     'force_logout_login'  =>  false,
